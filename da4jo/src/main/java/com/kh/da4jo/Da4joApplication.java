ebd385da9a4f692f3acb7aed9f089c3162715ea2
Wing-Shal <dhstlf2000@gmail.com>
2024-03-08 17:24:59 +0900

    다4조 프로젝트 생성

---
+++ da4jo/src/main/java/com/kh/da4jo/Da4joApplication.java
@@ -0,0 +1,13 @@
+package com.kh.da4jo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Da4joApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Da4joApplication.class, args);
+	}
+
+}
---
+++ da4jo/src/test/java/com/kh/da4jo/Da4joApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.da4jo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Da4joApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
