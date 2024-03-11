dd141894090568d68f80382d6e82227d4ed715b1
HW5434 <l990628l@gmail.com>
2024-03-11 13:04:55 +0900

    ReviewMapper 수정 및 Review,Qna Dao 생성

---
+++ da4jo/src/main/java/com/kh/da4jo/dao/QnaDao.java
@@ -0,0 +1,17 @@
+package com.kh.da4jo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class QnaDao {
+
+	//작성
+	
+	//목록
+	
+	//검색
+	
+	//수정
+	
+	//삭제
+}
---
+++ da4jo/src/main/java/com/kh/da4jo/dao/ReviewDao.java
@@ -0,0 +1,21 @@
+package com.kh.da4jo.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReviewDao {
+
+	@Autowired JdbcTemplate jdbcTemplate;
+	
+	//작성
+	
+	//목록
+	
+	//상세
+	
+	//검색
+	
+	//수정
+}
