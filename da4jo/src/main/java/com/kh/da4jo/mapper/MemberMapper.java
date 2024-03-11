649e576c0a3c8dcad566ee3180c00a69b831beab
bbongnam777 <bbongnam777@naver.com>
2024-03-11 11:03:07 +0900

    memberMapper 생성

--- da4jo/src/main/java/com/kh/da4jo/mapper/MemberMapper.java
+++ da4jo/src/main/java/com/kh/da4jo/mapper/MemberMapper.java
@@ -21,10 +21,20 @@ public class MemberMapper implements RowMapper<MemberDto>{
 		memberDto.setMemberPw(rs.getString("member_pw"));
 		memberDto.setMemberNameKor(rs.getString("member_name_kor"));
 		memberDto.setMemberNameEng(rs.getString("member_name_eng"));
+		memberDto.setMemberEmail(rs.getString("member_email"));
 		memberDto.setMemberContact1(rs.getString("member_contact1"));
 		memberDto.setMemberContact2(rs.getString("member_contact2"));
 		memberDto.setMemberBirth(rs.getDate("member_birth"));
+		memberDto.setMemberClearanceId(rs.getString("member_clearance_id"));
+		memberDto.setMemberZipcode(rs.getString("member_zipcode"));
+		memberDto.setMemberAddress1(rs.getString("member_address1"));
+		memberDto.setMemberAddress2(rs.getString("member_address2"));
+		memberDto.setMemberLevel(rs.getString("member_level"));
+		memberDto.setMemberJoinDate(rs.getDate("member_join_date"));
+		memberDto.setMemberLoginDate(rs.getDate("member_login_date"));
+		memberDto.setMemberCredit(rs.getInt("member_credit"));
+		memberDto.setMemberPoint(rs.getInt("member_point"));
+		memberDto.setMemberBlock(rs.getString("member_block"));
 		return memberDto;
 	}
-
 }
