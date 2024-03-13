6a76b37b0eeb7662a73b1688aab317462535bea6
simcat13 <rycbar0@gmail.com>
2024-03-13 13:07:52 +0900

    공지 기본기능만 구현

--- da4jo/src/main/java/com/kh/da4jo/dao/NoticeDao.java
+++ da4jo/src/main/java/com/kh/da4jo/dao/NoticeDao.java
@@ -3,6 +3,7 @@ package com.kh.da4jo.dao;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -15,6 +16,7 @@ public class NoticeDao {
 	private JdbcTemplate jdbcTemplate;
 	@Autowired
 	private NoticeMapper noticeMapper;
+	@Autowired
 
 	// C(create)
 	// noticeNo 시퀀스 미리 뽑는코드
@@ -34,48 +36,39 @@ public class NoticeDao {
 
 	// R(read) 공지목록
 	public List<NoticeDto> selectList() {// 공지의 단순 목록만 나오게
-		String sql = "select "
-				+ "notice_no, notice_title, notice_content, " 
-				+ "notice_writer, notice_wdate, notice_vcount "
-				+ "from notice order by notice_no desc";
+		String sql = "select " + "notice_no, notice_title, notice_content, "
+				+ "notice_writer, notice_wdate, notice_vcount " + "from notice order by notice_no desc";
 		return jdbcTemplate.query(sql, noticeMapper);
 	}
+
 	// 공지의 검색 목록이 나옴
 	public List<NoticeDto> selectList(String column, String keyword) {
-		String sql = "select "
-					+ "notice_no, notice_title, notice_content, " 
-					+ "notice_writer, notice_wdate, notice_vcount "
-					+ "from notice "
-					+ "where instr(" + column + ", ?) > 0 "
-					+ "order by notice_no desc";
+		String sql = "select " + "notice_no, notice_title, notice_content, "
+				+ "notice_writer, notice_wdate, notice_vcount " + "from notice " + "where instr(" + column + ", ?) > 0 "
+				+ "order by notice_no desc";
 		Object[] data = { keyword };
 		return jdbcTemplate.query(sql, noticeMapper, data);
 	}
 
+	// 단일조회
 	public NoticeDto selectOne(int noticeNo) {// 상세페이지
 		String sql = "select * from notice where notice_no = ?";
 		Object[] data = { noticeNo };
 		List<NoticeDto> list = jdbcTemplate.query(sql, noticeMapper, data);
 		return list.isEmpty() ? null : list.get(0);
 	}
-	
+
 	// 수정
 	public boolean update(NoticeDto noticeDto) {
-		String sql = "update notice "
-						+ "set "
-							+ "notice_title=?, notice_content=?, "
-							+ "where notice_no=?";
-		Object[] data = {
-			noticeDto.getNoticeTitle(), noticeDto.getNoticeContent()
-		};
+		String sql = "update notice " + "set " + "notice_title=?, notice_content=? " + "where notice_no=?";
+		Object[] data = { noticeDto.getNoticeTitle(), noticeDto.getNoticeContent(), noticeDto.getNoticeNo() };
 		return jdbcTemplate.update(sql, data) > 0;
 	}
-	
-	
+
 	// D(delete) 삭세
 	public boolean delete(int noticeNo) {
 		String sql = "delete notice where notice_no = ?";
-		Object[] data = {noticeNo};
+		Object[] data = { noticeNo };
 		return jdbcTemplate.update(sql, data) > 0;
 	}
 
