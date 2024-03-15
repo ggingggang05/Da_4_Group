77873e52e001eb8dce0284ee2a5fd8fd7e60c823
bbongnam777 <bbongnam777@naver.com>
2024-03-15 19:06:36 +0900

    블락회원 구현 , 본인 글만 수정

--- da4jo/src/main/java/com/kh/da4jo/interceptor/BlockInterceptor.java
+++ da4jo/src/main/java/com/kh/da4jo/interceptor/BlockInterceptor.java
@@ -1,7 +1,11 @@
 package com.kh.da4jo.interceptor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.kh.da4jo.dao.MemberDao;
+import com.kh.da4jo.dto.MemberDto;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,16 +14,21 @@ import jakarta.servlet.http.HttpSession;
 
 @Service
 public class BlockInterceptor implements HandlerInterceptor{
-
+	
+	@Autowired
+	private MemberDao memberDao;
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		
 		HttpSession session = request.getSession();
-		String memberBlock = (String)session.getAttribute("memberBlock");
+		String loginId = (String)session.getAttribute("loginId");
+		MemberDto findDto =  memberDao.selectOne(loginId);
+		String memberBlock = findDto.getMemberBlock();
 		
-		if(memberBlock == "N") {//일반회원
-			return true;
+	    if ("N".equals(memberBlock)) { // 일반 회원
+	        return true;
 		}
 		else {//블락회원
 			response.sendError(403);
