aaf61791e97b2bba5fbe8d207e6eb70f5d5834e7
Wing-Shal <dhstlf2000@gmail.com>
2024-03-15 17:16:08 +0900

    차단회원 관리 기능 구현

--- da4jo/src/main/java/com/kh/da4jo/dao/MemberBlockDao.java
+++ da4jo/src/main/java/com/kh/da4jo/dao/MemberBlockDao.java
@@ -1,12 +1,16 @@
 package com.kh.da4jo.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.kh.da4jo.dto.MemberBlockDto;
 import com.kh.da4jo.dto.MemberDto;
+import com.kh.da4jo.mapper.BlockMemberVOMapper;
 import com.kh.da4jo.mapper.MemberBlockMapper;
+import com.kh.da4jo.vo.BlockMemberVO;
 
 @Repository
 public class MemberBlockDao
@@ -15,6 +19,8 @@ public class MemberBlockDao
 	JdbcTemplate jdbcTemplate;
 	@Autowired
 	MemberBlockMapper memberBlockMapper;
+	@Autowired
+	private BlockMemberVOMapper blockMemberVOMapper;
 	
 	public void insertMemberBlock(MemberBlockDto memberBlockDto ) {
 		String sql = "INSERT INTO MEMBER_BLOCK(BLOCK_NO, BLOCK_MEMBER_ID, BLOCK_STATUS, BLOCK_REASON) "
@@ -27,4 +33,32 @@ public class MemberBlockDao
 		Object[] datas = {memberDto.getMemberBlock(), memberDto.getMemberId()};
 		jdbcTemplate.update(sql, datas);
 	}
+	public List<MemberBlockDto> blockList(String memberId) {
+		String sql = "SELECT * FROM MEMBER_BLOCK WHERE BLOCK_MEMBER_ID=?";
+		return jdbcTemplate.query(sql, memberBlockMapper, memberId);
+	}
+	// BLOCK된 아이디 목록 조회
+		public List<BlockMemberVO> selectBlockListById() {
+			String sql = "SELECT SUB.MEMBER_ID, SUB.MEMBER_NAME_KOR, SUB.BLOCK_STATUS, SUB.BLOCK_TIME, SUB.BLOCK_REASON FROM " +
+                    "(SELECT M.MEMBER_ID, M.MEMBER_NAME_KOR, MB.BLOCK_STATUS, MB.BLOCK_TIME, MB.BLOCK_REASON, " +
+                    "ROW_NUMBER() OVER (PARTITION BY M.MEMBER_ID ORDER BY MB.BLOCK_TIME DESC) AS ROW_NUM " +
+                    "FROM MEMBER M " +
+                    "JOIN MEMBER_BLOCK MB ON M.MEMBER_ID = MB.BLOCK_MEMBER_ID " +
+                    "WHERE M.MEMBER_BLOCK = 'Y' ) SUB " +
+                    "WHERE SUB.ROW_NUM = 1";
+			return jdbcTemplate.query(sql, blockMemberVOMapper);
+		}
+		// BLOCK된 아이디 검색 조회
+		public List<BlockMemberVO> selectBlockListById(String memberId)
+		{
+			String sql = "SELECT SUB.MEMBER_ID, SUB.MEMBER_NAME_KOR, SUB.BLOCK_STATUS, SUB.BLOCK_TIME, SUB.BLOCK_REASON FROM " +
+                    "(SELECT M.MEMBER_ID, M.MEMBER_NAME_KOR, MB.BLOCK_STATUS, MB.BLOCK_TIME, MB.BLOCK_REASON, " +
+                    "ROW_NUMBER() OVER (PARTITION BY M.MEMBER_ID ORDER BY MB.BLOCK_TIME DESC) AS ROW_NUM " +
+                    "FROM MEMBER M " +
+                    "JOIN MEMBER_BLOCK MB ON M.MEMBER_ID = MB.BLOCK_MEMBER_ID " +
+                    "WHERE M.MEMBER_BLOCK = 'Y' AND INSTR(M.MEMBER_ID, ?) > 0) SUB " +
+                    "WHERE SUB.ROW_NUM = 1";
+			return jdbcTemplate.query(sql, blockMemberVOMapper, memberId);
+		}
+	
 }
