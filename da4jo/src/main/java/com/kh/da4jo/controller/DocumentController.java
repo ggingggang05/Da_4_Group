cb4f2f3ef89eb5a6490b25e50089b85e5dedbd25
Wing-Shal <dhstlf2000@gmail.com>
2024-03-20 10:07:01 +0900

    document컨트롤러 생성

---
+++ da4jo/src/main/java/com/kh/da4jo/controller/DocumentController.java
@@ -0,0 +1,14 @@
+package com.kh.da4jo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/document")
+public class DocumentController
+{
+	@RequestMapping("/buying-info")
+	public String buyingInfo() {
+		return "/WEB-INF/views/document/buying-info.jsp";
+	}
+}
